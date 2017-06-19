a502b3ebb803b351f342afe8981c06f792cd2f22
EisenbergEffect <rob@bluespire.com>
2017-06-18 21:03:31 -0700

    app(home): update template for home page

--- lib/app/src/screens/home-screen.ts
+++ lib/app/src/screens/home-screen.ts
@@ -1,34 +1,15 @@
 import {HttpClient} from 'aurelia-fetch-client';
-import {autoinject, transient} from 'aurelia-dependency-injection';
+import {autoinject} from 'aurelia-dependency-injection';
 import {InlineViewStrategy} from 'aurelia-templating';
 import {join} from 'aurelia-path';
 import {Location} from '../configuration';
-import 'http://blog.aurelia.io/shared/ghost-url.min.js?v=7013f8d3ca';
-
-declare var ghost: any;
-ghost.init({
-  clientId: "ghost-frontend",
-  clientSecret: "92ecab236b7e"
-});
 
 @autoinject
-@transient()
 export class HomeScreen {
   item: Location = null;
   strategy: InlineViewStrategy;
-  blog;
 
-  constructor(private http: HttpClient) {
-    this.http.fetch(ghost.url.api('posts', {limit: 5}))
-      .then(response => response.json())
-      .then((blog: any) => {
-        blog.posts.forEach(item => {
-           let d = new Date(item.published_at);
-           item.published_at = `${d.getFullYear()}-${d.getMonth()}-${d.getDate()}`;
-        });
-        this.blog = blog;
-      });
-  }
+  constructor(private http: HttpClient) {}
 
   withItem(item: Location) {
     this.item = item;
