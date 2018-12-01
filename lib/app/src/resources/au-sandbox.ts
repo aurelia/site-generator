216ff3c0112bcd4e14417e90aa528aff79ae825e
Rob Eisenberg <rob@bluespire.com>
2018-12-01 10:18:32 -0800

    feat(app): ensure codemirror on sandboxes

--- lib/app/src/resources/au-sandbox.ts
+++ lib/app/src/resources/au-sandbox.ts
@@ -10,6 +10,12 @@ export class AuSandbox {
   constructor(private element: Element) {}
 
   bind() {
+    // Ensure we always use CodeMirror for inline demos.
+    // It's much lighter-weight than Monico.
+    if (this.src.indexOf('codemirror=1') === -1) {
+      this.src += '&codemirror=1';
+    }
+
     this.element.innerHTML = `<iframe src="${this.src}" style="width:100%; height:500px; border:0; border-radius: 4px; border: 2px solid rgb(36, 40, 42); overflow:hidden;" sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"></iframe>`;
   }
 }
